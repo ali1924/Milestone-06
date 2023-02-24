b72f0b3d47391330f26ab37b33cf2e9cf0d0c3e2
ali1924 <mohammadali229987@gmail.com>
2023-02-24 21:59:42 +0600

    modal created

--- api examples/js/meal.js
+++ api examples/js/meal.js
@@ -17,14 +17,21 @@ const displayMeals = (meals) => {
     const mealDiv = document.createElement("div");
     mealDiv.innerHTML = `
             <div class="col">
-            <div class="card h-100">
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">${meal.strMeal}</h5>
-                <p class="card-text">This is a short card.</p>
+                <div class="card h-100">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">This is a short card.</p>
+
+                    <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                    Details
+                    </button>
+
+                    </div>
                 </div>
             </div>
-            </div>
+
+
         `;
     mealContainer.appendChild(mealDiv);
   });
@@ -36,6 +43,23 @@ const searchMeals = () => {
     loadMeals(searchText);
     
 }
+const loadMealDetail = idMeal => {
+    // console.log('csdb')
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+}
+const displayMealDetails = meal => {
+    // console.log(meals);
+    document.getElementById("mealDetailsLabel").innerText = meal.strMeal;
+    const mealDetails = document.getElementById("mealDetailsBody");
+    mealDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    `;
+}
+
 loadMeals('');
 
 
