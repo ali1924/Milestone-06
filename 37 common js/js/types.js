56e8044b2c788519e075f542fbf8f333ad467e7a
ali1924 <mohammadali229987@gmail.com>
2023-02-27 21:05:18 +0600

    37-1 Module Overview, Primitive and non-primitive data type

---
+++ 37 common js/js/types.js
@@ -0,0 +1,34 @@
+// strongly typed language
+// int a=5;
+// string name='Ali';
+// bool b=true;
+// object obj={name:'md'};
+// int [] array=[10,20,3];
+// string [] names=['a','bbbb'];
+
+
+// js is a dynamic typed language
+
+// primtive types
+const num = 50;
+const bol = true;
+console.log(typeof num, typeof bol);
+
+// non-primtive
+const array = [10, 20, 20];
+const name = 'badsvbadvbqw';
+const obj = { name: 'ali' };
+console.log(typeof array, typeof name, typeof obj);
+
+
+// 
+let x = 5;
+let y = x;
+console.log(x, y);
+// re-assign 
+y = 9;
+console.log(x, y);
+let p = { job: 'back end' };
+let q = p;
+q.job = 'fronr end';
+console.log(p, q);
